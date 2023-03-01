00a7a77b26294600409b7549572cc221735cef83
Amanda Ross <amikaross@gmail.com>
2023-03-01 10:50:40 -0700

    Add model relationships and validations testing and implementation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+  has_many :subscriptions
+  validates :first_name,
+            :last_name,
+            :email,
+            :address,
+            presence: true
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,10 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates :title,
+            :price,
+            :status,
+            :frequency,
+            presence: true
 end
